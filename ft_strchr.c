f50abbaf1516e81106907e81ebb27d907838ed05
TheGloomist <woodchips.picture@gmail.com>
2022-04-01 16:56:16 +0200

    strlcat ruining my life

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,19 @@
 /*   By: izaitcev <izaitcev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/25 19:01:55 by izaitcev      #+#    #+#                 */
-/*   Updated: 2022/03/25 19:01:56 by izaitcev      ########   odam.nl         */
+/*   Updated: 2022/04/01 16:55:29 by izaitcev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+
+}
+int	main()
+{
+	char	str[] = "123,45";
+	char	find = ",";
+	
+}
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,24 +6,41 @@
 /*   By: izaitcev <izaitcev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/25 19:03:53 by izaitcev      #+#    #+#                 */
-/*   Updated: 2022/03/29 18:42:56 by izaitcev      ########   odam.nl         */
+/*   Updated: 2022/04/01 16:49:18 by izaitcev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 // #include <stdio.h>
 // #include <string.h>
-// #include "libft.h"
+#include "libft.h"
 
-// size_t ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
-// {
-//     int	i;
-// 	int	
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	dst_len;
+	size_t	src_len;
+	size_t	i;
+	size_t	dst_pos;
 
-// }
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	i = 0;
+	dst_pos = dst_len;
+	if (dst_len < dstsize - 1 && dstsize > 0)
+	{
+		while (src[i] && dst_len + 1 > dstsize - 1)
+		{
+			dst[dst_pos] = src[i];
+			dst_pos++;
+			i++;
+		}
+	}
+	if (dst_len >= dstsize)
+		dst_len = dstsize;
+	return (dst_len + src_len);
+}
 
 // int main()
 // {
-//     char str1[] = "dst string";
-//     char str2[] = "src string";
-
+// 	char str1[] = "dst string";
+// 	char str2[] = "src string";
 // }
