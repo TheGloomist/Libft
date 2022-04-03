eb2ae4e13f77cefec46235d122bed9c9ae366cb1
TheGloomist <woodchips.picture@gmail.com>
2022-04-04 01:39:05 +0200

    strncmp is kinda working, but only returns 0, -1 or 1. tester no likey

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,12 +6,12 @@
 /*   By: izaitcev <izaitcev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/26 17:31:57 by izaitcev      #+#    #+#                 */
-/*   Updated: 2022/04/04 00:03:43 by izaitcev      ########   odam.nl         */
+/*   Updated: 2022/04/04 01:35:00 by izaitcev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 #include "libft.h"
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
@@ -21,24 +21,22 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n && s1[i] && s2[i])
 	{
-		if ((unsigned char)(s1[i]) == (unsigned char)(s2[i])) // if s1 and s2 are equal
-			return (0);
-		else if ((unsigned char)(s1[i]) < (unsigned char)(s2[i])) // first character that doesn't match has a lower value in s1 compared to s2
+		if ((unsigned char)(s1[i]) < (unsigned char)(s2[i]))
 			return (-1);
-		else if ((unsigned char)(s1[i]) > (unsigned char)(s2[i])) // first character that doesn't match has a greater value in s1 compared to s2
+		else if ((unsigned char)(s1[i]) > (unsigned char)(s2[i]))
 			return (1);
 		i++;
 	}
-	return (-42);
+	return (0);
 }
 
-int	main()
-{
-	char	str1[] = "lmao";
-	char	str2[] = "mmao";
-	int		comp;
+// int	main()
+// {
+// 	char	str1[] = "zmao";
+// 	char	str2[] = "lmao";
+// 	int		comp;
 
-	comp = ft_strncmp(str1, str2, 4);
-	printf("%i\n", comp);
+// 	comp = ft_strncmp(str1, str2, 4);
+// 	printf("%i\n", comp);
 
-}
+// }
