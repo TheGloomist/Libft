cf7b4e74bc5515a01490d0d7c614c198a62f7f78
TheGloomist <woodchips.picture@gmail.com>
2022-04-04 07:03:03 +0200

    Started on strnstr. Meh

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,36 @@
 /*   By: izaitcev <izaitcev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/29 15:51:37 by izaitcev      #+#    #+#                 */
-/*   Updated: 2022/03/29 15:51:39 by izaitcev      ########   odam.nl         */
+/*   Updated: 2022/04/04 03:37:37 by izaitcev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+
+	if (!needle)
+		return (haystack);
+	
+
+}
+
+int	main()
+{
+	char	haystack[] = "Things are only impossible until they're not";
+	char	needle[] = "only";
+	char	*result;
+
+	result = strnstr(haystack, needle, 20);
+	printf("%s\n", result);
+	result = ft_strnstr(haystack, needle, 10);
+	printf("%s\n", result);
+	
+	return(0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: izaitcev <izaitcev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/25 19:09:19 by izaitcev      #+#    #+#                 */
-/*   Updated: 2022/04/04 02:18:29 by izaitcev      ########   odam.nl         */
+/*   Updated: 2022/04/04 02:54:53 by izaitcev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *str);
