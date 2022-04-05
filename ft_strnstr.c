e958c5ebe0da90e9f6a64fb6037761ffc432ba7e
TheGloomist <woodchips.picture@gmail.com>
2022-04-05 19:41:16 +0200

    Strnstr seems to be working, but still gotta fix a few things

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,36 +6,49 @@
 /*   By: izaitcev <izaitcev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/29 15:51:37 by izaitcev      #+#    #+#                 */
-/*   Updated: 2022/04/04 03:37:37 by izaitcev      ########   odam.nl         */
+/*   Updated: 2022/04/05 19:39:19 by izaitcev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <string.h>
+// #include <stdio.h>
+// #include <string.h>
 #include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	i;
+	size_t	h_i;
+	size_t	n_i;
 
-	i = 0;
-
-	if (!needle)
-		return (haystack);
-	
-
+	if (!haystack)
+		return (NULL);
+	if (!needle[0])
+		return ((char *)haystack);
+	h_i = 0;
+	n_i = 0;
+	while (haystack[h_i])
+	{
+		while (haystack[h_i + n_i] == needle[n_i] && h_i + n_i < len)
+		{
+			if (!haystack[h_i + n_i] && !needle[n_i])
+				return ((char *)haystack + h_i);
+			n_i++;
+		}
+		if (!needle[n_i])
+			return ((char *)haystack + h_i);
+		h_i++;
+	}
+	return (NULL);
 }
 
-int	main()
-{
-	char	haystack[] = "Things are only impossible until they're not";
-	char	needle[] = "only";
-	char	*result;
+// int	main()
+// {
+// 	char	haystack[] = "Things are only impossible until they're not";
+// 	char	needle[] = "only";
+// 	char	*result;
 
-	result = strnstr(haystack, needle, 20);
-	printf("%s\n", result);
-	result = ft_strnstr(haystack, needle, 10);
-	printf("%s\n", result);
-	
-	return(0);
-}
+// 	// result = strnstr(haystack, needle, 20);
+// 	// printf("%s\n", result);
+// 	result = ft_strnstr(haystack, needle, 20);
+// 	printf("%s\n", result);
+// 	return(0);
+// }
