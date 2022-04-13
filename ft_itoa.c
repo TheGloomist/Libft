a0efc3cdf0f9be21e1461015df47c16ecf112bad
TheGloomist <woodchips.picture@gmail.com>
2022-04-13 20:27:16 +0200

    Meh

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,26 +6,74 @@
 /*   By: izaitcev <izaitcev@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/25 18:57:26 by izaitcev      #+#    #+#                 */
-/*   Updated: 2022/03/29 15:52:54 by izaitcev      ########   odam.nl         */
+/*   Updated: 2022/04/13 19:27:21 by izaitcev      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "libft.h"
 
-// int     check_neg(int n)
-// {
-//     if (n < 0)
-//         return(-1);
-//     return(1);
-// }
+int     check_neg(int n)
+{
+    if (n < 0)
+        return(-1);
+    return(1);
+}
 
-// char    *ft_itoa(int n)
-// {
-//     char    *str;
-//     int     neg;
+int		get_length(int n)
+{
+	int	len;
 
-//     neg = check_neg(n);
-    
+	len = 0;
+	if (n < 0)
+		len++;
+	while (n > 0)
+	{
+		n = n / 10;		
+		len++;
+	}
+	return (len);
+}
 
-//     return(str);
-// }
+char    *ft_itoa(int n)
+{
+    char    *str;
+    int     neg;
+	int		i;
+	int		len;
+
+	len = get_length(n);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return(NULL);
+    neg = check_neg(n);
+	i = 0;
+	if (neg == -1)
+	{
+		str[i] = '-';
+		i++;
+	}
+	while (i < len)
+	{
+		str[i] = (n % 10) + '0';
+		i++;
+		n = n / 10;
+	}
+	str[i] = '\0';
+    return(str);
+}
+
+int	main()
+{
+	int		num = 12345;
+	int		len;
+	char	*str;
+
+	str = ft_itoa(num);
+	// len = get_length(num);
+	// printf("%i\n", len);
+	printf("%s\n", str);
+
+	return (0);
+}
